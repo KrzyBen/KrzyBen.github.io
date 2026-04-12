69ebcdc8babcd0c53c48a31eb8c21abf1cd77af2
KrzyBen <benjamin.manriquez.ramirez@gmail.com>
2026-04-12 12:13:18 -0400

    EDIT - Cambios en en astro.config.mjs para el css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://KrzyBen.github.io',
-  base: '/KrzyBen.github.io',
+  base: '/KrzyBen.github.io/',
+
+  build: {
+    inlineStylesheets: 'always'
+  },
 
   vite: {
     plugins: [tailwindcss()],

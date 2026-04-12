7ebc37030a194040303e4fa425f292ccfb6089ba
KrzyBen <benjamin.manriquez.ramirez@gmail.com>
2026-04-12 11:25:44 -0400

    ADD , EDIT & DEL - Se añaden archivos para crear la pagina que presente el proyecto de manera dividida. Se Eliminan elementos inecesarios.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://KrzyBen.github.io',
-    base: '/KrzyBen.github.io',
+  site: 'https://KrzyBen.github.io',
+  base: '/KrzyBen.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
